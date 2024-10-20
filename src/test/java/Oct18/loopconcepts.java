091f71f8956645fc7a71f91b7585c1ac6ab7d0cd
Rajesh0395 <rajesht0395@gmail.com>
2024-10-20 15:46:26 +0530

    second push while loop program

---
+++ src/test/java/Oct18/loopconcepts.java
@@ -0,0 +1,16 @@
+package Oct18;
+
+public class loopconcepts {
+
+    public static void main(String[] args) {
+        int a = 12;
+        while (a < 20) {
+            if (a % 2 == 0) {
+                System.out.println("the value of a is:" + a);
+                break;
+            }
+        }
+    }
+}
+
+
