ae4389923257faf958cecec1f681242a9ec339a1
Rajesh0395 <rajesht0395@gmail.com>
2024-11-13 06:51:41 +0530

    exercise programs

--- src/test/java/Oct14/exerciseprograms.java
+++ src/test/java/Oct14/exerciseprograms.java
@@ -36,14 +36,14 @@ public class exerciseprograms {
         System.out.println(m/n+n);
 
         //fifth problem
-        int a=6;
-        int b=3;
-        int c=2;
+        int aaa=6;
+        int bbb=3;
+        int ccc=2;
         //                 6+3*2-3
         //                  6+6-3
         //                 12-3
         //                  9
-        System.out.println(a+b*c-a/c);
+        System.out.println(aaa+bbb*ccc-aaa/ccc);
 
         //sixth problem
         int total=100;
@@ -53,11 +53,92 @@ public class exerciseprograms {
         System.out.println(total-discount/2);
 
         //seventh problem
-        int x=10;
-        int y=5;
+        int xxx=10;
+        int yyy=5;
          //               ((10-5)*(15)
         //                (5)*15
         //75
-        System.out.println((x-y)*(x+y));
-    }
+        System.out.println((xxx-yyy)*(xxx+yyy));
+
+
+        //Eight problem
+        int a=5;
+        int b=3;
+        int c=3;
+        //we do not consider decimal
+        //              (5*3+3-1)
+        //              (15+3-1)
+        //               18-1
+        //17
+        System.out.println(a*b+c-a/b);
+// 9th problem
+        int d=15;
+        int e=5;
+                         //(15/5*5(8)
+        //               15/5*40
+        //3*40
+        //120
+        //  15/5*(5+3)
+        //    5*8
+
+        // correct solution
+        //             (15/5*(8)
+        //               3*8
+        //24
+        System.out.println(d/e*(e+3));
+
+        //10th problem
+
+        int f=8;
+        int g=4;
+        int h=2;
+        //System.out.println(a/b+c*(b-c));
+        System.out.println(f/g+h*(g-h));
+        //                8/4+2*2
+        //                2+4
+        //6
+// 11th problem
+
+        int i=20;
+        int j=4;
+        System.out.println(i-j*3+(j*2));
+                          //20-4*3+8
+        //                  20-12+8
+        //                  28-12
+                               //16
+        //12th problem
+
+        int x=4;
+        int y=6;
+        int z=2;
+        System.out.println(x+y*z-x*z);
+         //               4+12-8
+        //                 16-8 //8
+
+        //13th problem
+        double ax =5.0;
+        double bx=2.0;
+        System.out.println (ax*bx+(ax-bx)/bx);
+        //                 (5.0*2.0+3.0/2.0
+       //                   5.0*2.0+1.5
+       //11.5
+
+       //14th problem
+
+       int u=3;
+       int v=9;
+       System.out.println(u*(v/u)+v%u);
+                           //3*3+0
+       //                    9+0=9
+
+       //15th problem
+
+        int k=9;
+        int l=2;
+       System.out.println(k*(l+1)-l);
+       //                 9* 3-2
+                       //   27-2= 25
+
+
+}
 }
