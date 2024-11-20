dc2b17856a54a096708aaff261b8f479d31a0a92
Rajesh0395 <rajesht0395@gmail.com>
2024-11-20 21:47:23 +0530

    exercise programs of arrays and strings

--- src/test/java/Nov4/ArrayExercise.java
+++ src/test/java/Nov4/ArrayExercise.java
@@ -4,44 +4,39 @@ import java.util.Arrays;
 
 public class ArrayExercise {
 
-    public static void main (String[] args)
-    {
-        int[] arr1={1,2,3,4};
-        /// arr2=arr1
+    public static void main(String[] args) {
+        /*int[] arr1 = {1, 2, 3, 4};
+        arr2=arr1
         //1,2,3,4=1,2,3,4
-      //  int[] arr2=arr1;
-        // arr2[2]--index position 2 =10
-        //so now arr2 value is {1,2,10,4}
-        //arr2[2]=10;
-        //arr2=arr1 so it will print 1,2,10,4
-        System.out.println(Arrays.toString(arr1));
+          int[] arr2=arr1;
+         arr2[2]--index position 2 =10
+        so now arr2 value is {1,2,10,4}
+        arr2[2]=10;
+        arr2=arr1 so it will print 1,2,10,4
+        System.out.println(Arrays.toString(arr1));*/
 
         //Second Exercise Program
-      /*  int [] arr={1,2,3,4};
+        /*int[] arr = {1, 2, 3, };
         {
-            //1=1*2=2
-            //  2=2*2=4
-            //3=3*2=6
-            //4=4*2=8
-
-            for (int val:arr)
-            {
-                val=val*2;
-               // System.out.println(val);
+            //
+           /* for (int val : arr) {
+                val = val * 13;
+                // System.out.println(val);
                 System.out.println(Arrays.toString(arr));
-            }
-        }*/
-        //third exercise program
+            }*/
+        //i<0
+        //0----1
+        //1--2
+
+  //Third Exercise Program
         int []arr={1,2,3,4,5,6};
-        {
-        //for (int i=0;j=arr.length-1;i<j;i+=2;j-=2)
+              // i=0, j=5-1;==j=4,i=0
+            for(int i=0,j=arr.length-1; i<j; i+=2,j-=2)
             {
-              //  int temp=arr[i];
-              //  arr[i]=arr[j];
-              //  arr[j]=temp;
-                System.out.println(Arrays.toString(arr));
-
-            }
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
         }
+        System.out.println(Arrays.toString(arr));
     }
-}
+    }
--- src/test/java/Oct16/stringexercise.java
+++ src/test/java/Oct16/stringexercise.java
@@ -1,23 +1,50 @@
 package Oct16;
 
+import java.sql.SQLOutput;
+
 public class stringexercise {
 
-    public static void main(String [] args)
-    {
-        String str="abcde".repeat(100);
-        //System.out.println(str);
-        System.out.println(str.charAt(450));
+    public static void main(String [] args) {
+        //First program
+        String s="abcde".repeat(100);
+        //System.out.println(s);
+        System.out.println(s.charAt(12));
+        //Second program
+        String Str="Counting Character is a String.";
+        System.out.println("Character at index 10:"+Str.charAt(10));
 
-        String str1="Counting characters in a string";
-        System.out.println("character at index 10:"+str1.charAt(10));
-        //excepting output c
+        //Third program
+        String Str1="hello world";
+        String Str2=Str1.substring(5);
+        System.out.println(Str2);
+        String Str3="world";
+        // world==world
+        System.out.println(Str2==Str3);
+       System.out.println(Str2.equals(Str3));
 
-        String str2="Lorem ipsum dolor sit amet, consectetur adipiscing elit.".repeat(5);
-       // System.out.println(str2);
-        System.out.println(str2.charAt(120));
+       //Fourth program
+        String str="concatenate";
+        //0=c ,0=1,n=2,c=3,a=4,t=5,e=6,n=7,a=8,t=9,e=10
+        //if substring used it will include the first value and last value will be excluded
+        str=str.substring(3,8).concat(str.substring(1,4).concat(str.substring(0,2)));
+        System.out.println(str);
 
-        String str3="ABCDEFGHJIKLMNOPQRSTUVWXYZ";
-        int randomIndex=(int)(Math.random()*str3.length());
-        //System.out.println(str3.charAt(randomIndex));
+        //Fifth program
+        //indexof will print the first character from index
+        String str5="hello java";
+        int index=str5.indexOf("java")-1;
+       // System.out.println(index-1);
+        System.out.println(str5.substring(index));
+
+        //Sixth program
+        String str6=" Welcome ";
+        System.out.println(str6.substring(0,0).isEmpty());
+        System.out.println(str6.substring(1,1).isBlank());
+
+        //Seventh program
+
+
+
+        //
     }
 }
